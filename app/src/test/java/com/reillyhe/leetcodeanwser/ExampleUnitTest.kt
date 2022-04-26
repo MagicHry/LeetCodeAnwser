d515abbed04b25caa6e8f743c5b9f39bedd0e37f
hesheng <hesheng@gaoding.com>
2022-04-26 21:47:16 +0800

    AC RandomCopy.kt 随机链表深拷贝问题

--- app/src/test/java/com/reillyhe/leetcodeanwser/ExampleUnitTest.kt
+++ app/src/test/java/com/reillyhe/leetcodeanwser/ExampleUnitTest.kt
@@ -1,5 +1,7 @@
 package com.reillyhe.leetcodeanwser
 
+import com.reillyhe.leetcodeanwser.randomcp.Node
+import com.reillyhe.leetcodeanwser.randomcp.RandomCopy
 import com.reillyhe.leetcodeanwser.twosum.TwoSum
 import org.junit.Test
 
@@ -23,5 +25,65 @@ class ExampleUnitTest {
         val target2 = 6
         val ret2 = TwoSum.twoSum(input2, target2)
         assertEquals(target2, input2[ret2!![0]]+input2[ret2!![1]])
+    }
+
+    @Test
+    fun random_copy_normal_test() {
+        val node1 = Node(3)
+        val node2 = Node(6)
+        val node3 = Node(17)
+        val node4 = Node(9)
+        val node5 = Node(6)
+        node1.apply {
+            next = node2
+            random = null
+        }
+        node2.apply {
+            next = node3
+            random = node4
+        }
+        node3.apply {
+            next = node4
+            random = node1
+        }
+        node4.apply {
+            next = node5
+            random = node4
+        }
+        node5.apply {
+            next = null
+            random = node3
+        }
+        val node = node1
+        val info = node.info()
+        System.out.println(info)
+        val copiedNode = RandomCopy.copyRandomList(node)
+        val copiedInfo = copiedNode!!.info()
+        assertEquals(info, copiedInfo)
+    }
+
+    @Test
+    fun random_copy_equalv_test() {
+        val node1 = Node(3)
+        val node2 = Node(3)
+        val node3 = Node(3)
+        node1.apply {
+            next = node2
+            random = null
+        }
+        node2.apply {
+            next = node3
+            random = node3
+        }
+        node3.apply {
+            next = null
+            random = node1
+        }
+        val node = node1
+        val info = node.info()
+        System.out.println(info)
+        val copiedNode = RandomCopy.copyRandomList(node)
+        val copiedInfo = copiedNode!!.info()
+        assertEquals(info, copiedInfo)
     }
 }
