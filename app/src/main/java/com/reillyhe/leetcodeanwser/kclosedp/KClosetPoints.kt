f6d42f635d2cbaf6b007151b7d8d101cd0c84c5d
hesheng <hesheng@gaoding.com>
2022-05-08 04:40:08 +0800

    AC KClosetPoints.kt K个最近点问题

--- app/src/main/java/com/reillyhe/leetcodeanwser/kclosedp/KClosetPoints.kt
+++ app/src/main/java/com/reillyhe/leetcodeanwser/kclosedp/KClosetPoints.kt
@@ -16,10 +16,29 @@ object KClosetPoints {
             pointDistanceLst.add(pointWidD)
         }
         //接下来，我们使用一个小根堆进行堆排序，当然，堆排序只需要迭代K次就可以了
+        littleRootHeapSort(pointDistanceLst, k)
+        //取后K个元素，就是距离远点最近的K个点了
+        val retArr = mutableListOf<IntArray>()
+        for (index in (pointDistanceLst.size-1).downTo(pointDistanceLst.size-k)) {
+            val indexInOriginArr = pointDistanceLst[index].indexInOriginArr
+            retArr.add(points[indexInOriginArr])
+        }
+        return retArr.toTypedArray()
     }
 
     private fun littleRootHeapSort(points: ArrayList<PointWithDis>, iterationCnt: Int) {
-
+        var iterCnt = 0
+        for (curNeedSortLstLenght in points.size.downTo(1)) {
+            iterCnt++
+            //进行迭代
+            organizeLittleRoot(points, curNeedSortLstLenght)
+            //完成后，调换前后元素位置
+            val tailNode = points[curNeedSortLstLenght-1]
+            points[curNeedSortLstLenght-1] = points[0]
+            points[0] = tailNode
+            //如果已经达到次数限制了，就停止，不用继续了
+            if (iterCnt == iterationCnt) return
+        }
     }
 
     private fun organizeLittleRoot(points: ArrayList<PointWithDis>, needSortLstLength: Int) {
@@ -30,6 +49,7 @@ object KClosetPoints {
         }
         for (curParentIndex in lastParentNodeIndex.downTo(0)) {
             //从最后一个parent节点开始往前不断调整subtree
+            organizeSubTree(points, needSortLstLength, curParentIndex)
         }
     }
 
@@ -48,14 +68,16 @@ object KClosetPoints {
                 else -> rightChildIndex
             }
             //和父节点对比
-            val parentNodeValue = points[curParentIndex].distance
-            if (parentIndex <= points[minChildIndex].distance) {
+            val parentNode = points[curParentIndex]
+            if (points[parentIndex].distance <= points[minChildIndex].distance) {
                 //如果还是父亲小，则不用迭代了，当前这棵树以及其下面的subtree都是完备的小根
                 return
             }
             //如果父亲大，则要进行交换，同时要探查被调换位置的孩子所构成的subtree的情况
             points[curParentIndex] = points[minChildIndex]
-            points[minChildIndex] = points[curParentIndex]
+            points[minChildIndex] = parentNode
+            //同时将指针移动到minChildIndex处，继续进行迭代
+            curParentIndex = minChildIndex
         }
     }
 
@@ -67,18 +89,20 @@ object KClosetPoints {
         val leftChildIndex = target * 2 + 1
         val rightChildIndex = target * 2 + 2
         if (leftChildIndex >= needSortLstLength && rightChildIndex >= needSortLstLength) return false
-        return false
+        return true
     }
 
     /**
      * 求解某一点到原点的欧几里得距离
      */
-    private fun calDistance(points: IntArray): Float {
+    private fun calDistance(points: IntArray): Double {
         if (points.size != 2) {
-            return 0f
+            return 0.0
         }
-        return sqrt(points[0].toFloat().pow(2) + points[1].toFloat().pow(2))
+        val xV = points[0].toDouble()
+        val yV = points[1].toDouble()
+        return Math.sqrt(Math.pow(xV, 2.0) + Math.pow(yV, 2.0))
     }
 
-    data class PointWithDis(val indexInOriginArr: Int, val distance: Float)
+    data class PointWithDis(val indexInOriginArr: Int, val distance: Double)
 }
