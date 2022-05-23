c5e3e6f092f8829ea61c6798d1d12076431dfd7d
hesheng <hesheng@gaoding.com>
2022-05-23 18:59:03 +0800

    ValidBST inmid

---
+++ app/src/main/java/com/reillyhe/leetcodeanwser/validbstree/TreeNode.kt
@@ -0,0 +1,6 @@
+package com.reillyhe.leetcodeanwser.validbstree
+
+class TreeNode {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
---
+++ app/src/main/java/com/reillyhe/leetcodeanwser/validbstree/ValidBST.kt
@@ -0,0 +1,8 @@
+package com.reillyhe.leetcodeanwser.validbstree
+
+object ValidBST {
+
+    fun isValidBST(root: TreeNode?): Boolean {
+        return true
+    }
+}
